922ba0915349d65c87e69ba9ed0111a10ac73818
Nicolas Gibaud <nicolas.gibaud7@gmail.com>
2020-11-12 14:39:09 +0100

    Up delay button timer to 1s

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -169,7 +169,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 125;
+  htim1.Init.Prescaler = 250;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 64000;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
